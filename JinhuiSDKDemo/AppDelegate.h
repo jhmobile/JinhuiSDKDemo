5247a24b226010d8e62047cb4e7098aea319ffa6
mshqiu <mshqiu@mshqiudeMacBook-Pro.local>
2019-08-27 14:12:34 +0800

    Initial Commit

---
+++ JinhuiSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JinhuiSDKDemo
+//
+//  Created by mshqiu on 2019/8/27.
+//  Copyright © 2019 mshqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JinhuiSDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JinhuiSDKDemo
+//
+//  Created by mshqiu on 2019/8/27.
+//  Copyright © 2019 mshqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
